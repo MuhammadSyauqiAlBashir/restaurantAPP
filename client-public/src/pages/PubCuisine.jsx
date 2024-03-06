fd3b69785ac89992f127419fc14405248784bda9
MuhammadSyauqiAlBashir <bashirsyauqi@gmail.com>
2024-03-06 23:49:19 +0700

    back to original card

--- client-public/src/pages/PubCuisine.jsx
+++ client-public/src/pages/PubCuisine.jsx
@@ -8,18 +8,18 @@ function PubCuisine({ state }) {
   console.log(state, "<<<<");
   return (
     <>
-    <div className="container my-5" style={{backgroundImage: 'url("https://res.cloudinary.com/dep5hbgsn/image/upload/v1709740884/hcogheg1hlzczuxouruz.jpg")', backgroundRepeat:"repeat"}}>
+    <div className="container my-5" style={{backgroundImage: 'url("https://wallsbyme.com/cdn/shop/products/textured_98367550_01-sw_500x.jpg?v=1628704626")', backgroundRepeat:"repeat"}}>
     <Navbar/>
     <Carousel/>
     <div className="container my-5">
     <img style={{width: "100%"}} src="https://res-console.cloudinary.com/dep5hbgsn/media_explorer_thumbnails/a99580c19d2a5942df8b8330ddb5359c/detailed" alt="" />
       <h2 className="text-center my-5">Available Food</h2>
-      <div>
+      <div className="row row-cols-4 g-3">
         {state.map((item) => (
           <Card item={item} key={item.id} />
         ))}
       </div>
-    </div>
+      </div>
     </div>
     </>
   );
