0610fca6165285b56eee845cf78f1fecfd0b080d
MuhammadSyauqiAlBashir <bashirsyauqi@gmail.com>
2024-03-07 15:21:26 +0700

    pub pagination sort filter done

---
+++ client-public/src/components/searchbar.jsx
@@ -0,0 +1,18 @@
+export default function SearchBar({handleInputChange}){
+  return (
+    <div className="input-group mb-3">
+      <span className="input-group-text">Find by Food Name :</span>
+      <div className="form-floating">
+        <input
+          type="text"
+          className="form-control"
+          id="floatingInputGroup1"
+          placeholder="Keyword"
+          name="search"
+          onChange={handleInputChange}
+        />
+        <label htmlFor="floatingInputGroup1">Keyword</label>
+      </div>
+    </div>
+  );
+}
--- client-public/src/pages/PubCuisine.jsx
+++ client-public/src/pages/PubCuisine.jsx
@@ -4,31 +4,50 @@ import Navbar from "../components/navbar";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import BASE_URL from "../constant";
+import SearchBar from "../components/searchbar";
 
 function PubCuisine() {
-  const [state, setState] = useState([]);
-  const [page, setPage] = useState("")
-  function handlingPage(number){
-    setPage(`?page[number]=${number}`)
-    console.log(`${BASE_URL}${page}`)
+  const [state, setState] = useState({ data: [] });
+  const [params, setParams] = useState({});
+  function handleInputChange(event) {
+    const { value } = event.target;
+    find(value);
+  }
+  function handleInputSort(event) {
+    const { value } = event.target;
+    sort(value);
+  }
+  function sort(value){
+    if (value === 1) {
+      setParams({...params, "sort" : "id,asc"})
+    }else{
+      setParams({...params, "sort" : "id,desc"})
+    }
+  }
+  function find(keyword) {
+    setParams({ ...params, search: keyword });
+  }
+  function handlingPage(number) {
+    setParams({ ...params, "page[number]": number });
   }
   async function FetchData() {
     try {
       const { data } = await axios({
         method: "get",
-        url: `${BASE_URL}${page}`,
+        url: `${BASE_URL}`,
+        params: params,
       });
-      setState(data.data);
+      setState(data);
     } catch (error) {
       console.log(error);
     }
   }
   useEffect(() => {
     FetchData();
-  }, [page]);
+  }, [params]);
   return (
     <>
-      <Navbar />
+      <Navbar FetchData={FetchData} />
       <div className="container my-5">
         <Carousel />
         <div className="container my-5">
@@ -38,20 +57,42 @@ function PubCuisine() {
             alt=""
           />
           <h2 className="text-center my-5">Available Food</h2>
+          <SearchBar handleInputChange={handleInputChange} />
+          <div className="form-floating mb-3">
+            <select
+              className="form-select"
+              id="floatingSelect"
+              aria-label="Floating label select example"
+              onChange={handleInputSort}
+            >
+              <option value="1">Oldest</option>
+              <option value="2">Latest</option>
+            </select>
+            <label htmlFor="floatingSelect">Sort By :</label>
+          </div>
           <div className="row row-cols-4 g-3">
-            {state.map((item) => (
+            {state.data.map((item) => (
               <Card item={item} key={item.id} />
             ))}
           </div>
         </div>
       </div>
       <nav aria-label="Page navigation example">
-      <ul class="pagination d-flex justify-content-center">
-        <li class="page-item"><button class="page-link" onClick={() => handlingPage(1)}>1</button></li>
-        <li class="page-item"><button class="page-link" onClick={() => handlingPage(2)}>2</button></li>
-        <li class="page-item"><button class="page-link" onClick={() => handlingPage(3)}>3</button></li>
-      </ul>
-    </nav>
+        <ul className="pagination d-flex justify-content-center">
+          {new Array(state.totalPage).fill(0).map((item, index) => {
+            return (
+              <li className="page-item" key= {index}>
+                <button
+                  className="page-link"
+                  onClick={() => handlingPage(index + 1)}
+                >
+                  {index + 1}
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
     </>
   );
 }
